aa3f2b94e84223333ed46e2302af41def15ff338
Preetam Patil <preetampatil@iisc.ac.in>
2020-06-24 23:03:54 +0530

    Added automated email reporting script

---
+++ server_code/daily_report.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, timedelta
+import pytz
+from db_access import insert_row, get_row, check_uniqueness, append_row_fb, visitor_count_percentile
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
+import json
+import random
+import time
+import uuid
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import os
+from dotenv import load_dotenv
+from email_send import gmail_send, send_gmails
+
+IST = pytz.timezone ("Asia/Kolkata")
+yest = timedelta(days=1)
+rdate = (datetime.now().astimezone(IST) - yest).strftime('%d-%b-%Y')
+print(rdate)
+email_sub = "Daily report (" + rdate + ") on Covid Readiness Indicator"
+email_body = "Please find attached the daily summary report for submissions to the web server.\n\n"
+email_body += "Dry run only, hence not attaching the report pptx yet.\n\n"
+email_body += "\n-COVID-19 Readiness Indicator team\n"
+email_recipients = ['pvp.iisc@gmail.com', 'contact.cni@iisc.ac.in', 'nihesh.7391@gmail.com']
+#attachment = ['analytics/workplace_dailyReport_18_06.pptx']
+#gmail_send(email_recipient, email_sub, email_body)
+
+send_gmails(email_recipients, email_sub, email_body)
+#send_gmails(email_recipients, email_sub, email_body, attachment)
+
--- server_code/email_send.py
+++ server_code/email_send.py
@@ -1,8 +1,10 @@
-# Copyright [2020] [Indian Institute of Science, Bangalore]
-# SPDX-License-Identifier: Apache-2.0
-
 import smtplib, ssl
 import os
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -18,3 +20,40 @@ def gmail_send(receiver_email, subject, body):
         server.login(smail, spass)
         server.sendmail(spass, receiver_email, message)
 
+def send_gmails(send_to, subject, body, files=None):
+    assert isinstance(send_to, list)
+
+    send_from = os.getenv("SENDER_EMAIL")
+    spass = os.getenv("SENDER_PASSWORD")
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    message = "Subject: " + subject + "\n\n" + body
+    context = ssl.create_default_context()
+    smail = os.getenv("SENDER_EMAIL")
+    spass = os.getenv("SENDER_PASSWORD")
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context) # Secure the connection
+        server.login(smail, spass)
+        server.sendmail(spass, send_to, msg.as_string())
+
+    #smtp = smtplib.SMTP(server)
+    #smtp.sendmail(send_from, send_to, msg.as_string())
+    #smtp.close()
